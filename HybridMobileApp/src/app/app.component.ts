4a800ba366cf6eaaa139ecc33dfb6cfc64501022
Shiva Kumar H R <shivahr@in.ibm.com>
2017-10-27 12:05:14 +0530

    Refactoring to improve code readability

--- HybridMobileApp/src/app/app.component.ts
+++ HybridMobileApp/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+
 @Component({
   templateUrl: 'app.html'
 })
@@ -11,22 +12,20 @@ export class MyApp {
   rootPage:any;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, renderer: Renderer) {
+    console.log('--> MyApp constructor() called');
+
     renderer.listenGlobal('document', 'mfpjsloaded', () => {
-      console.log('--> MFP API init complete');
-      this.MFPInitComplete();
+      console.log('--> MyApp mfpjsloaded');
+      this.rootPage = HomePage;
     })
 
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      console.log('--> MyApp platform.ready() called');
       statusBar.styleDefault();
       splashScreen.hide();
     });
-  }
-
-  MFPInitComplete() {
-    console.log('--> MFPInitComplete() function called');
-    this.rootPage = HomePage;
   }
 
 }
